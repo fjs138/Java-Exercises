34e0653cd19409bb9f2081e0266ae7590fcd87c5
Frank Santaguida <fjs138@gmail.com>
2016-01-29 14:26:44 -0500

    Initial commit

---
+++ BeerSong/src/BeerSong.java
@@ -0,0 +1,35 @@
+import java.lang.String;
+import java.lang.System;
+
+public class BeerSong {
+
+    public static void main (String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+            if (beerNum == 1) {
+                word = "bottle"; //singular form, as in ONE bottle
+            }
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println("Take one down.") ;
+            System.out.println("Pass it around." ) ;
+            beerNum = beerNum - 1;
+
+            if (beerNum >= 1) {
+                if (beerNum == 1){
+                    word = "bottle";
+                }
+                System.out.println(beerNum + " " + word + " of beer on the wall");
+            }
+            if (beerNum < 1) {
+                System.out.println("No more bottles of beer on the wall.");
+            } //end else
+
+        } //end while
+
+
+    } //end main
+
+} //end class
