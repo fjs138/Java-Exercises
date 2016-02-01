59597df8789b5d3a9b184011a3129e6e7720f1d6
Frank Santaguida <fjs138@gmail.com>
2016-01-31 23:50:19 -0500

    working on PoolPuzzleOne

--- PoolPuzzle/src/PoolPuzzleOne.java
+++ PoolPuzzle/src/PoolPuzzleOne.java
@@ -5,7 +5,31 @@ public class PoolPuzzleOne {
     public static void main (String [] args) {
         int x = 0; // compile test
 
-        while (_________) {
+        while ( x < 4 ) {
+
+            System.out.print("a");
+            if (x < 1 ) {
+                System.out.print(" ");
+            } // end of x<1 if
+            System.out.print("n");
+
+            if ( x > 1 ) {
+
+                System.out.print(" oyster");
+                x = x + 2;
+
+            } // end of x>1 if
+
+            if ( x == 1 ) {
+                System.out.print("noys");
+            } // end of x==1 if
+            if ( x < 1) {
+                System.out.print("oise");
+
+            }
+            System.out.print(" ");
+
+            x = x + 1;
 
         } // end of while loop
 
